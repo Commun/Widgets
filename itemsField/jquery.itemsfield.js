25518c2c9448aa870b7736103eaa24f33a8ce9ef
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-05-04 16:47:55 -0400

    Better

--- itemsField/jquery.itemsfield.js
+++ itemsField/jquery.itemsfield.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 	
 (function($) {
+	
 	$.widget( "ui.itemsField", {
 		
 		options: { 
@@ -222,31 +223,28 @@
 		
 		_itemRemove: function( e ) {
 			e.preventDefault();
-			 $(e.target).parents('.'+this.widgetBaseClass+'-item').eq(0).fadeOut('fast',function() {
+			$(e.target).parents('.'+this.widgetBaseClass+'-item').eq(0).fadeOut('fast',function() {
 				$(this).remove(); 
-			 });
+			});
 		},
 		
 		// Use the _setOption method to respond to changes to options
 		_setOption: function( key, value ) {
+			
 			switch( key ) {
 				case "clear":
-				// handle changes to clear option
+					
 				break;
 			}
 			
-			// In jQuery UI 1.8, you have to manually invoke the _setOption method from the base widget
 			$.Widget.prototype._setOption.apply( this, arguments );
-			// In jQuery UI 1.9 and above, you use the _super method instead
 			this._super( "_setOption", key, value );
 		},
 		
-		// Use the destroy method to clean up any modifications your widget has made to the DOM
 		destroy: function() {
-			// In jQuery UI 1.8, you must invoke the destroy method from the base widget
 			$.Widget.prototype.destroy.call( this );
-			// In jQuery UI 1.9 and above, you would define _destroy instead of destroy and not call the base method
 		}
+		
 	});
 	
 }(jQuery));
